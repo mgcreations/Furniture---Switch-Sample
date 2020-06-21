c8c6bdc26c250fd1367a6a864fe3325072b88f56
MG Creations <info@mgcreations.co.za>
2020-06-21 22:12:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char woodFinish;
+
+            Console.Write("Choose the wood for the table order \n P for Pine, O for Oak or M for Mahogany:");
+            woodFinish = Convert.ToChar(Console.ReadLine().ToUpper());
+
+            switch (woodFinish)
+            {
+                case 'P':
+                    Console.WriteLine("Pine tables cost $100");
+                    break;
+                case 'O':
+                    Console.WriteLine("Oak tables cost $225");
+                    break;
+                case 'M':
+                    Console.WriteLine("Mahogany cost $310");
+                    break;
+                default:
+                    Console.WriteLine("Price is $0");
+                    break;
+            }
+        }
+    }
+}
